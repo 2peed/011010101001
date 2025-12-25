e6aa9be2d77483439b43aff05115cdf0889f3ecc
Speed <melihdeniz087@gmail.com>
2025-12-25 23:20:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def home():
     return "", 200
 
 def run_web():
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 threading.Thread(target=run_web).start()
